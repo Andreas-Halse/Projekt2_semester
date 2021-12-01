d8df79df835805d43b004fa309b20d30f34d3292
HGPlays <legoandreas@gmail.com>
2021-12-01 16:45:13 +0100

    Vi tester

---
+++ Test/Test.ino
@@ -0,0 +1,20 @@
+/*
+ Name:		Test.ino
+ Created:	12/1/2021 4:44:44 PM
+ Author:	andreashalse
+*/
+
+
+
+//Dette er en test.
+
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
