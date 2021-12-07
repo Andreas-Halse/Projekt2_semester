aa2b73561f9a99c60b24fc2954a207a7e333984e
Jzjepsen <95351040+Jzjepsen@users.noreply.github.com>
2021-12-07 11:59:20 +0100

    slavechild

---
+++ Slave_Child/Slave_Child/Slave_Child.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		Slave_Child.ino
+ Created:	12/7/2021 11:58:48 AM
+ Author:	juliezepernickjepsen
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
