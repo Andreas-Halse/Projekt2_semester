f5213e2befa4eab11f884c3c0a1dba057529fc95
Jzjepsen <95351040+Jzjepsen@users.noreply.github.com>
2021-12-11 17:28:15 +0100

    Domain for Master

---
+++ Domain_Adr_Fct/Domain_Adr_Fct/AdresseOgFunktion.h
@@ -0,0 +1,24 @@
+#pragma once
+/// @brief domain class for data on addresses and funktion on x.10
+/// 
+/// 
+class AdresseOgFunktion
+{
+private:
+	//Slaves adresse databits
+	const int SlaveChildAdr_[8]			= {1,0,0,1,1,0,0,1};
+	const int SlaveCurtainAdr_[8]		= {1,0,0,1,0,1,1,0};
+	const int SlaveLightAdr_[8]			= {0,1,1,0,0,1,1,0};
+
+	//Funktionskald databits
+	const int ActivateFct_[16]			= {1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0};
+	const int DectivateFct_[16]			= {0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1};
+	const int RollUpFct_[16]			= {1,0,0,1,1,0,0,1,0,1,0,1,0,1,0,1};
+	const int RollDownFct_[16]			= {0,1,1,0,0,1,1,0,1,0,1,0,1,0,1,0};
+	const int LightsOnFct[16]			= {1,0,1,0,0,1,0,1,1,0,1,0,0,1,0,1};
+	const int LightsOffFct[16]			= {0,1,0,1,1,0,1,0,0,1,0,1,1,0,1,0};
+	const int IncreaseBrightness_[16]	= {1,0,1,0,1,0,1,0,1,0,1,0,0,1,0,1};
+	const int DecreaseBrightness_[16]	= {0,1,0,1,0,1,0,1,0,1,0,1,1,0,1,0};
+	const int Stop[16]					= {1,0,1,0,1,0,1,0,0,1,0,1,0,1,0,1};
+};
+
