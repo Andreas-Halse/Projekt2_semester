2ceca861626e6b32db989b0d6acb3f43e6d504cd
HGPlays <81820733+HGPlays@users.noreply.github.com>
2021-12-12 14:50:16 +0100

    Noter

--- Motor/Motor_Module/src/main.cpp
+++ Motor/Motor_Module/src/main.cpp
@@ -9,30 +9,29 @@
 int main()
 {
 
-Serial.begin(9600);
+Serial.begin(9600); // sætter Baudrate
 pinMode(53, OUTPUT); // sætter pin 53 til output til on
 pinMode(51, OUTPUT); // sætter pin 51 til output til retning
-Motor_module M;
+
+Motor_module M; // opretter object
 
 for(;;)
 {
+// Dette er en test main.
 
+M.MotorOn(); // vi tænder motor
+M.CurtainUp(); // sætter retning
 
+_delay_ms(4000); // laver delay (4 sekunder) mens motor køre
 
-M.MotorOn();
-M.CurtainUp();
+M.MotorOff(); // slukker motor
 
-_delay_ms(4000);
+_delay_ms(5000); // venter 5 sekunder
 
-M.MotorOff();
+M.MotorOn(); // tænder motor
+M.CurtainDown(); // retning ændret
 
-_delay_ms(5000);
+_delay_ms(10000); // venter 10 sekunder
 
-
-M.MotorOn();
-M.CurtainDown();
-
-_delay_ms(10000);
 }
-
 }
