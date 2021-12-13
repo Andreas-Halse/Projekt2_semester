cfc7f42c6d91bb5854b0f76fe8683968df4e92e9
Magnus Kappel Petersen <magnus.kappel@icloud.com>
2021-12-13 18:22:28 +0100

    Lys

--- Lys/Lys.ino
+++ Lys/Lys.ino
@@ -1,15 +1,79 @@
 /*
  Name:		Lys.ino
  Created:	12/12/2021 3:57:29 PM
- Author:	kappel
+ Author:	
 */
 
-// the setup function runs once when you press reset or power the board
+#include <avr/io.h>
+
+using namespace std;
+
+int volatile intensitet = 220;
+
+
 void setup() {
-
+	pinMode(13, OUTPUT); //sætter 13 til output
 }
 
-// the loop function runs over and over again until power down or reset
+
 void loop() {
-  
+
+		turnOn();
+		_delay_ms(1000);
+		decreaseBrightness();
+		_delay_ms(1000);
+		decreaseBrightness();
+		_delay_ms(1000);
+		decreaseBrightness();
+		_delay_ms(1000);
+		decreaseBrightness();
+		_delay_ms(1000);
+		decreaseBrightness();
+		_delay_ms(1000);
 }
+
+void turnOn() {
+	if (intensitet <= 0)
+	{
+		intensitet = 220;
+		analogWrite(13, intensitet);
+	}
+	else
+	{
+		analogWrite(13, intensitet);
+	}
+}
+
+void turnOff() 
+{
+	analogWrite(13, LOW);
+}
+
+void increaseBrightness() {
+
+	if (intensitet <= 220)
+	{
+		intensitet = intensitet + 55;
+	}
+	else
+	{
+		intensitet = 220;
+	}
+	
+	analogWrite(13, intensitet);
+}
+
+void decreaseBrightness() {
+
+	if (intensitet >= 55) 
+	{
+		intensitet = intensitet - 55;
+		analogWrite(13, intensitet);
+	} 
+	else 
+	{
+		intensitet = 0;
+	}
+}
+
+
