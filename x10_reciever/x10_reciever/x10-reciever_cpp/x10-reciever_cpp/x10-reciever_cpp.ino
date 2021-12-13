f71a4ef5eabfea51f88905e4cfde8078b3f9d9ea
oscarboe <95352280+oscarboe@users.noreply.github.com>
2021-12-13 18:21:29 +0100

    de2 board

--- x10_reciever/x10_reciever/x10-reciever_cpp/x10-reciever_cpp/x10-reciever_cpp.ino
+++ x10_reciever/x10_reciever/x10-reciever_cpp/x10-reciever_cpp/x10-reciever_cpp.ino
@@ -1,22 +1,113 @@
-/*
- Name:		x10_reciever_cpp.ino
- Created:	12/13/2021 1:01:51 PM
- Author:	oscar
-*/
+#include "x10_reciever.h"
 
-#include <avr/io.h>
-int volatile x = 0;
-// the setup function runs once when you press reset or power the board
 
-void setup() {
-	pinMode(13, OUTPUT);
-	x = 70;
+
+void x10_reciever::loadIntoArray(int arr)
+{
+	if (digitalRead(53) == HIGH)//if datapin high
+	{
+		//Serial.print("Test1\n");
+
+		arr[i] = 1;		//load in i
+		i++;			//count up databit counter
+		zeroCounter = 0;//reset zerocounter
+	}
+	else if (digitalRead(53) == LOW)
+	{
+		//Serial.print("Test2\n");
+		arr[i] = 0;
+		i++;
+		zeroCounter++;	// count up zerocounter
+	}
 }
 
-// the loop function runs over and over again until power down or reset
-void loop() {
-	digitalWrite(13, HIGH);
-	_delay_us(100);
-	digitalWrite(13, LOW);
-	_delay_us(100 - x);
+ISR(INT3_vect)
+{
+
+	if (oneCounter >= 3)
+	{
+		if (i <= (SIZE + 1))
+		{
+			Serial.println(digitalRead(53));
+			if (arrayBool)		// if true load into messageArray if false compareArray
+			{
+				loadIntoArray(messageArray);
+				if (zeroCounter >= 3)		//counts number of zeroes to check for stopbits
+				{
+					//Serial.print("zeroCounter overflow\n  ");
+					arrayBool = false;		//switch input array
+					zeroCounter = 0;		//reset zerocounter
+					i = 0;					// reset databit recieved counter to ensure
+					oneCounter = 0;
+				}
+			}
+			else								//Same thing
+			{
+				loadIntoArray(compareArray);
+				if (zeroCounter >= 3)
+				{
+					//Serial.print("zeroCounter overflow  AGAIN \n");
+					zeroCounter = 0;
+					arrayBool = true;
+					messageDone = true;
+					i = 0;
+					oneCounter = 0;
+				}
+			}
+		}
+		else
+		{
+			Serial.print("array too small");
+		}
+	}
+	else if (digitalRead(53) == HIGH)
+	{
+		oneCounter++;
+		if (arrayBool)
+		{
+			loadIntoArray(messageArray);
+		}
+		else
+		{
+			loadIntoArray(compareArray);
+		}
+
+	}
+}
+
+void x10_reciever::readData()
+{
+	if (i <= (SIZE + 1))
+	{
+		Serial.println(digitalRead(53));
+		if (arrayBool)		// if true load into messageArray if false compareArray
+		{
+			loadIntoArray(_messageArray);
+			if (zeroCounter >= 3)		//counts number of zeroes to check for stopbits
+			{
+				//Serial.print("zeroCounter overflow\n  ");
+				arrayBool = false;		//switch input array
+				zeroCounter = 0;		//reset zerocounter
+				i = 0;					// reset databit recieved counter to ensure
+				oneCounter = 0;
+			}
+		}
+		else								//Same thing
+		{
+			loadIntoArray(_compareArray);
+			if (zeroCounter >= 3)
+			{
+				//Serial.print("zeroCounter overflow  AGAIN \n");
+				zeroCounter = 0;
+				arrayBool = true;
+				messageDone = true;
+				i = 0;
+				oneCounter = 0;
+			}
+		}
+	}
+	else
+	{
+		Serial.print("array too small");
+	}
 }
